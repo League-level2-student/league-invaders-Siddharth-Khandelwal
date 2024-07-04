a1f21f879d28d0d949d2a2c12da61c6502f8a393
Siddharth <sidkhandelwal2010@gmail.com>
2024-07-03 19:56:35 -0700

    League Invaders: Runner and Game Backround done from VIEW part of recipe

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,25 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	JFrame frame = new JFrame();
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel bob;
 
+	public LeagueInvaders() {
+		bob = new GamePanel();
+	}
+
+	public static void main(String[] args) {
+		LeagueInvaders tom = new LeagueInvaders();
+		tom.setup();
+
+	}
+
+	void setup() {
+		frame.add(bob);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
