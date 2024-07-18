6c16b270c1741da3ec6a0c1d54fd32e9845c1c89
Siddharth <sidkhandelwal2010@gmail.com>
2024-07-17 20:01:13 -0700

    Commit fix: everything till Model Management done,movement smoother

---
+++ src/ObjectManager.java
@@ -0,0 +1,42 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ObjectManager {
+	Rocketship rob;
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+	Random random = new Random();
+
+	public ObjectManager(Rocketship r) {
+		rob = r;
+	}
+
+	void addProjectile(Projectile p) {
+		projectiles.add(p);
+	}
+
+	void addAlien() {
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
+	}
+
+	void update() {
+		rob.update();
+		for (int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).update();
+		}
+		for (int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).update();
+		}
+	}
+
+	void draw(Graphics g) {
+		rob.draw(g);
+		for (int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).draw(g);
+		}
+		for (int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).draw(g);
+		}
+	}
+}
